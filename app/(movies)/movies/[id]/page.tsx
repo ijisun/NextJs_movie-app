c4277b2137c87d154d1e624582c0781527420ebc
Leejisun <ijisun9292@gmail.com>
2025-09-09 22:34:18 +0900

    fix: params 타입 및 비동기 처리 수정으로 빌드 오류 해결

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -3,7 +3,7 @@ import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
 interface IParams {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }
 
 export async function generateMetadata({ params }: IParams) {
@@ -18,10 +18,10 @@ export default async function HomePage({ params }: IParams) {
   const { id } = await params;
   return (
     <div>
-      <Suspense fallback={"<h1>Loading Movie info</h1>"}>
+      <Suspense fallback={<h1>Loading Movie info</h1>}>
         <MovieInfo id={id} />
       </Suspense>
-      <Suspense fallback={"<h1>Loading Movie videos</h1>"}>
+      <Suspense fallback={<h1>Loading Movie videos</h1>}>
         <MovieVideos id={id} />
       </Suspense>
     </div>

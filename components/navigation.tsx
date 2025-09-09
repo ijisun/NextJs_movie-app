4c6f7dcf5d195b45d186f620b54e0d68f721f69d
Leejisun <ijisun9292@gmail.com>
2025-09-09 23:38:30 +0900

    Navigation About Us 버튼 Go Back 버튼으로 변경

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,19 +1,22 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 import styles from "../styles/nav.module.css";
 
 export default function Navigation() {
-  const path = usePathname();
+  const router = useRouter();
+  const goBack = () => {
+    router.back();
+  };
   return (
     <nav className={styles.nav}>
       <ul>
         <li>
-          <Link href="/">Home</Link> {path === "/" && "❤"}
+          <Link href="/">Home</Link>
         </li>
         <li>
-          <Link href="/about-us">About Us</Link> {path === "/about-us" && "❤"}
+          <button onClick={goBack}>Go Back</button>
         </li>
       </ul>
     </nav>

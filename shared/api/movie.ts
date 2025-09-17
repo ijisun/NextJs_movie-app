96bd4eb8ede9bbf4f17d47a1e525427b26b57952
Leejisun <ijisun9292@gmail.com>
2025-09-18 02:25:03 +0900

    id 타입을 number → string으로 수정

--- shared/api/movie.ts
+++ shared/api/movie.ts
@@ -13,27 +13,27 @@ export async function fetchAllMovies(): Promise<MovieList[]> {
   return response.json();
 }
 
-export async function fetchMovie(id: number): Promise<MovieDetail> {
+export async function fetchMovie(id: string): Promise<MovieDetail> {
   const response = await fetch(`${API_URL}/${id}`);
   return response.json();
 }
 
-export async function fetchMovieVideos(id: number): Promise<MovieVideo[]> {
+export async function fetchMovieVideos(id: string): Promise<MovieVideo[]> {
   const response = await fetch(`${API_URL}/${id}/videos`);
   return response.json();
 }
 
-export async function fetchMovieCredits(id: number): Promise<MovieCredit[]> {
+export async function fetchMovieCredits(id: string): Promise<MovieCredit[]> {
   const response = await fetch(`${API_URL}/${id}/credits`);
   return response.json();
 }
 
-export async function fetchMovieProviders(id: number): Promise<MovieProviders> {
+export async function fetchMovieProviders(id: string): Promise<MovieProviders> {
   const response = await fetch(`${API_URL}/${id}/providers`);
   return response.json();
 }
 
-export async function fetchSimilarMovies(id: number): Promise<SimilarMovies[]> {
+export async function fetchSimilarMovies(id: string): Promise<SimilarMovies[]> {
   const response = await fetch(`${API_URL}/${id}/similar`);
   return response.json();
 }

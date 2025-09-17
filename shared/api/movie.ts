3eaa1a1affd1b1af919daae70268fe7ad8c2c232
Leejisun <ijisun9292@gmail.com>
2025-09-18 01:03:43 +0900

    API fetch 함수에 반환 타입 명시

--- shared/api/movie.ts
+++ shared/api/movie.ts
@@ -1,31 +1,39 @@
 import { API_URL } from "../../app/constants";
+import {
+  MovieList,
+  MovieDetail,
+  MovieVideo,
+  MovieCredit,
+  SimilarMovies,
+  MovieProviders,
+} from "../../types/movie";
 
-export async function fetchAllMovies() {
+export async function fetchAllMovies(): Promise<MovieList[]> {
   const response = await fetch(API_URL);
   return response.json();
 }
 
-export async function fetchMovie(id: string) {
+export async function fetchMovie(id: number): Promise<MovieDetail> {
   const response = await fetch(`${API_URL}/${id}`);
   return response.json();
 }
 
-export async function fetchMovieVideos(id: string) {
+export async function fetchMovieVideos(id: number): Promise<MovieVideo[]> {
   const response = await fetch(`${API_URL}/${id}/videos`);
   return response.json();
 }
 
-export async function fetchMovieCredits(id: string) {
+export async function fetchMovieCredits(id: number): Promise<MovieCredit[]> {
   const response = await fetch(`${API_URL}/${id}/credits`);
   return response.json();
 }
 
-export async function fetchMovieProviders(id: string) {
+export async function fetchMovieProviders(id: number): Promise<MovieProviders> {
   const response = await fetch(`${API_URL}/${id}/providers`);
   return response.json();
 }
 
-export async function fetchSimilarMovies(id: string) {
+export async function fetchSimilarMovies(id: number): Promise<SimilarMovies[]> {
   const response = await fetch(`${API_URL}/${id}/similar`);
   return response.json();
 }
